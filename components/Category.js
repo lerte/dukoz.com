828f383fe5ae2b6730d23e86cb5d96232a7ec24f
lerte smith <lerte@qq.com>
2023-03-01 19:50:30 +0800

    update

---
+++ components/Category.js
@@ -0,0 +1,13 @@
+export default function Category() {
+  return (
+    <section className="relative overflow-hidden p-3 md:p-5 text-center opacity-100">
+      <div className="box-border overflow-hidden absolute top-0 left-0 bottom-0 right-0">
+        <img
+          alt="hero image"
+          src="/fill.webp"
+          className="absolute inset-0 box-border object-cover"
+        />
+      </div>
+    </section>
+  )
+}

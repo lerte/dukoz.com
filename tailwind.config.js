e2c26f83be19af058fec9bdebf35fec63297ad86
lerte smith <lerte@qq.com>
2023-03-01 18:34:28 +0800

    replace ts to js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#102B6A', // 主色调
+        secondary: '#426AB3', // 次颜色
+        success: '#1D953F',
+        danger: '#ED1941',
+        warning: '#FFC20E',
+        info: '#4e72b8',
+        light: '#999D9C',
+        dark: '#130c0e'
+      }
+    }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }

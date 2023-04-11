0b390d6ff8ef32f77fa1b43c858a558f1adaae4b
Lerte Smith <lertesmith@gmail.com>
2023-04-11 10:43:56 +0800

    更新注册页面

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
+import Layout from '@/layouts/Stacked'
+import Button from '@/components/Button'
 
-function SignUpView() {
+export default function SignUpView() {
   const router = useRouter()
 
   const [loading, setLoading] = useState(false)
@@ -29,7 +31,7 @@ function SignUpView() {
       }
 
       if (session) {
-        router.replace('/profile')
+        router.reload('/dashboard')
       } else {
         setSuccess(`We sent a verification link to ${email.value}`)
         setError(null)
@@ -46,43 +48,42 @@ function SignUpView() {
   }
 
   return (
-    <section className="flex h-96 flex-col items-center justify-center gap-4">
-      <form
-        className="flex w-full flex-col gap-2 md:w-96"
-        onSubmit={handleSignUp}
-      >
-        <h1 className="self-start text-3xl font-bold">Create an account</h1>
-        {success && (
-          <div className="bg-green-500 p-2 text-white">{success}</div>
-        )}
-        {error?.map(({ message }) => (
-          <div key={message} className="bg-red-600 p-2 text-[13px] text-white">
-            <p>{message}</p>
-          </div>
-        ))}
-
-        <input type="text" placeholder="Type your name" />
-        <input type="email" placeholder="Type your email" />
-        <input
-          autoComplete="new-password"
-          type="password"
-          placeholder="Type your password"
-        />
-        <div className="flex justify-between gap-4">
-          <Link className="text-indigo-600" href="/sign-in">
-            Already have an account?
-          </Link>
-          <button
-            type="submit"
-            className="shrink-0 border border-gray-500 py-2 px-3 transition hover:bg-gray-500 hover:text-white"
-            disabled={loading}
+    <Layout>
+      <div className="relative mx-auto flex h-full w-full max-w-screen-2xl items-center overflow-y-auto bg-gray-50 dark:bg-gray-900">
+        <section className="mx-auto w-fit rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+          <form
+            className="flex w-full flex-col gap-2 md:w-96"
+            onSubmit={handleSignUp}
           >
-            Register
-          </button>
-        </div>
-      </form>
-    </section>
+            <h1 className="self-start text-3xl font-bold">Create an account</h1>
+            {success && (
+              <div className="bg-green-500 p-2 text-white">{success}</div>
+            )}
+            {error?.map(({ message }) => (
+              <div
+                key={message}
+                className="bg-red-600 p-2 text-[13px] text-white"
+              >
+                <p>{message}</p>
+              </div>
+            ))}
+
+            <input type="text" placeholder="Type your name" />
+            <input type="email" placeholder="Type your email" />
+            <input
+              autoComplete="new-password"
+              type="password"
+              placeholder="Type your password"
+            />
+            <div className="flex justify-between gap-4">
+              <Link className="text-indigo-600" href="/sign-in">
+                Already have an account?
+              </Link>
+              <Button loading={loading}>Register</Button>
+            </div>
+          </form>
+        </section>
+      </div>
+    </Layout>
   )
 }
-
-export default SignUpView

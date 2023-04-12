aafaadcee914e29076ba3ab9645ae48f2e7114a8
Lerte Smith <lertesmith@gmail.com>
2023-04-12 20:07:14 +0800

    公共主页管理页面

--- pages/api/meta/pageShowList.js
+++ pages/api/meta/pageShowList.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   // 获取授权的主页
   const { userID, accessToken } = req.query
   const response =
-    await fetch(`${FACEBOOK_GRAPH_URL}/${userID}/accounts?fields=name,cover,fan_count,followers_count,access_token&
+    await fetch(`${FACEBOOK_GRAPH_URL}/${userID}/accounts?fields=name,cover,fan_count,followers_count,rating_count,is_owned,access_token&
     access_token=${accessToken}`)
   const result = await response.json()
   res.status(200).json(result)
--- pages/permission.js
+++ pages/permission.js
@@ -1,8 +1,10 @@
 import Layout from '@/layouts/Sidebar'
 import { useState, useEffect } from 'react'
-import FacebookPage from '@/components/FacebookPage'
+import HiTrash from '@/components/HeroIcons/HiTrash'
+import { Button, Card, Table, ToggleSwitch } from 'flowbite-react'
 
 export default function Permission({ user }) {
+  const [open, setOpen] = useState(false)
   const [pages, setPages] = useState([])
 
   const getPages = async ({ userId, userAccessToken }) => {
@@ -24,11 +26,61 @@ export default function Permission({ user }) {
 
   return (
     <Layout user={user}>
-      <section className="grid grid-cols-1 space-y-12 md:grid-cols-2 md:gap-6 md:gap-x-6 md:space-y-0 lg:grid-cols-3">
-        {pages.map((page) => (
-          <FacebookPage key={page.id} page={page} />
-        ))}
-      </section>
+      <Card>
+        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+          公共主页管理
+        </h5>
+        <Table hoverable={true}>
+          <Table.Head>
+            <Table.HeadCell>主页ID</Table.HeadCell>
+            <Table.HeadCell>主页名称</Table.HeadCell>
+            <Table.HeadCell>BM</Table.HeadCell>
+            <Table.HeadCell>Facebook个号</Table.HeadCell>
+            <Table.HeadCell>负责人</Table.HeadCell>
+            <Table.HeadCell>授权状态</Table.HeadCell>
+            <Table.HeadCell>主页评分</Table.HeadCell>
+            <Table.HeadCell>点赞</Table.HeadCell>
+            <Table.HeadCell>粉丝</Table.HeadCell>
+            <Table.HeadCell>差评隐藏</Table.HeadCell>
+            <Table.HeadCell>操作</Table.HeadCell>
+          </Table.Head>
+
+          <Table.Body className="divide-y">
+            {pages.map((page) => (
+              <Table.Row
+                key={page.id}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              >
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {page.id}
+                </Table.Cell>
+                <Table.Cell>{page.name}</Table.Cell>
+                <Table.Cell></Table.Cell>
+                <Table.Cell>{user.meta.userId}</Table.Cell>
+                <Table.Cell>{user.meta.userName}</Table.Cell>
+                <Table.Cell>{page.is_owned ? '有效' : '无效'}</Table.Cell>
+                <Table.Cell>{page.rating_count || '暂无评分'}</Table.Cell>
+                <Table.Cell>{page.fan_count}</Table.Cell>
+                <Table.Cell>{page.followers_count}</Table.Cell>
+                <Table.Cell>
+                  <ToggleSwitch
+                    checked={open}
+                    color="success"
+                    onChange={() => setOpen(!open)}
+                    label="差评隐藏"
+                  />
+                </Table.Cell>
+                <Table.Cell>
+                  <Button color="failure">
+                    <HiTrash />
+                    移除
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </Card>
     </Layout>
   )
 }

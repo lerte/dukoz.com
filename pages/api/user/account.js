b7c62bbd9bdedb690700cfdd56160173bd96bbd9
Lerte Smith <lertesmith@gmail.com>
2023-04-20 22:14:16 +0800

    修复facebook授权

--- pages/api/user/account.js
+++ pages/api/user/account.js
@@ -4,14 +4,16 @@ export default async function handler(req, res) {
   // 保存用户的facebook账号信息
   const { _id, userId, userName, userAccessToken } = JSON.parse(req.body)
 
-  const { data, errors } = await altogic.db
-    .model('users.meta')
-    .filter(`_parent == "${_id}"`)
-    .updateFields([
-      { field: 'userId', updateType: 'set', value: userId },
-      { field: 'userName', updateType: 'set', value: userName },
-      { field: 'userAccessToken', updateType: 'set', value: userAccessToken }
-    ])
+  await altogic.db.model('users.meta').filter(`_parent == "${_id}"`).delete()
+
+  const { data, errors } = await altogic.db.model('users.meta').set(
+    {
+      userId,
+      userName,
+      userAccessToken
+    },
+    _id
+  )
 
   if (errors) {
     res.status(errors.status).json({ errors })
